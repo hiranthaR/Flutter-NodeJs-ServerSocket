5de99de392e275e62ca2bd29177c1782163b7696
hirantha <sahanhirantha@gmail.com>
2019-03-07 18:40:13 +0530

    mobile app is connecting

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_socket_io/flutter_socket_io.dart';
+import 'package:flutter_socket_io/socket_io_manager.dart';
+
 
 void main() => runApp(MyApp());
 
@@ -18,11 +21,52 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+
+  _MyHomePageState(){
+//    getConnection();
+  }
+
+
+  void getConnection() async {
+    SocketIO socketIO = SocketIOManager().createSocketIO("http://10.0.2.2:3000", "/",socketStatusCallback: _socketStatus);
+    socketIO.init();
+    socketIO.connect();
+    socketIO.sendMessage("sex", "{msg:'comming from a flutter socket!'}");
+//    socketIO.subscribe(event, callback)
+
+  }
+
+  _socketStatus(dynamic data) {
+    print("Socket status: " + data);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Socket test app"),
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            RaisedButton(
+              onPressed: getConnection,
+              child: Text("connect"),
+            ),
+//            StreamBuilder(
+//              stream: socket,
+//              builder: (context, snapshot) {
+//                return Padding(
+//                  padding: const EdgeInsets.symmetric(vertical: 24.0),
+//                  child: Text(snapshot.hasData
+//                      ? '${String.fromCharCodes(snapshot.data)}'
+//                      : ''),
+//                );
+//              },
+//            ),
+          ],
+        )
       ),
     );
   }
